6df5766bed086ac6e81c6b908e89fd0672660bb4
LOYCORP\YCAO <ycao@loyalty.com>
2017-08-16 16:14:25 -0400

    works

--- server.js
+++ server.js
@@ -6,40 +6,17 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.static(path.resolve(__dirname, './react-ui/build/static')));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './react-ui/build', 'index.html'));
+});
 
-
-// Priority serve any static files.
-
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-
-
-// Answer API requests.
-
-app.get('/api', function (req, res) {
-
-  res.set('Content-Type', 'application/json');
-
-    res.send('{"message":"Hello from the custom server!"}');
-
-    });
-
-
-
-    // All remaining requests return the React app, so it can handle routing.
-
-    app.get('*', function(request, response) {
-
-      response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-
-      });
-
-
+const PORT = process.env.PORT || 3000;
 
       app.listen(PORT, function () {
 
         console.log(`Listening on port ${PORT}`);
 
 	});
+//????
