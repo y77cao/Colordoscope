d9b47573d56eb5bf529ca3151004b5e81a9a9106
LOYCORP\YCAO <ycao@loyalty.com>
2017-08-16 14:58:12 -0400

    ahhhh

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('.react-ui/build'));
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './react-ui/build', 'index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../react-ui/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
